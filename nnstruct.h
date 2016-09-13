eafcd6ed48443882b888ba5abbf27e28dc45df99
sankarsanseal <sankarsanseal@icloud.com>
2016-09-13 22:15:13 +0530

    First Commit.

---
+++ nnstruct.h
@@ -0,0 +1,18 @@
+/*
+ * nnstruct.h
+ *
+ *  Created on: 21 আগস্ট, 2016
+ *      Author: sankarsan
+ */
+
+#ifndef NNSTRUCT_H_
+#define NNSTRUCT_H_
+
+#define NO_OF_PARAMETER 3
+typedef struct x1_x2
+{
+	float x[NO_OF_PARAMETER];
+	int ylabel;
+} twoDem;
+
+#endif /* NNSTRUCT_H_ */
